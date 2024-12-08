c2592e0411fdce6e3894d976212ada7070641b38
Gustavo Barreto <gustavobarretodaf@gmail.com>
2024-12-08 11:18:29 -0300

    Initial Commit

---
+++ RainFy/RainFyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainFyApp.swift
+//  RainFy
+//
+//  Created by Gustavo on 08/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RainFyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
